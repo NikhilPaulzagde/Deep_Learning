4a70de4c4ccd267bb4629dfd5d06e9e08a198fb8
NikhilPaulzagde <118707097+NikhilPaulzagde@users.noreply.github.com>
2023-09-01 11:50:32 +0530

    Final commit

---
+++ Lemon Quality Detection/lemon/app.py
@@ -0,0 +1,61 @@
+# save this as app.py
+from flask import Flask, request, render_template
+import os
+import numpy as np
+import tensorflow as tf
+from keras.models import load_model
+from keras.utils import load_img, img_to_array
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# model = load model("models/fruits.h5")
+model=load_model("models/lemon.h5")
+ 
+class_name =['bad_quality', 'empty_background', 'good_quality']
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+    if request.method == 'POST':
+        f = request.files['fruit']
+        filename= f.filename
+        target = os.path.join(APP_ROOT, 'images/')
+        # print(target)
+        des = "/".join([target, filename])
+        f.save(des)
+
+        test_image= load_img('images\\'+filename,target_size=(300,300))
+        test_image = img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)
+        prediction = model.predict(test_image)
+        # print(prediction)
+
+        predicted_class= class_name[np.argmax(prediction[0])]
+        # print(predicted_class)
+        confidence = round(np.max(prediction[0])*100)
+        # print(confidence)
+
+        return render_template("prediction.html", confidence= "Chances -> "+str(confidence)+ "%", prediction = "Prediction -> "+str(predicted_class))
+
+    else:
+        return render_template("prediction.html")
+
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
